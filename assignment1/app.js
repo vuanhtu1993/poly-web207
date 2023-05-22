0025d19d2e1e7323a822abde9e4254d9b72fffcb
tuva9 <tuva9@fpt.edu.vn>
2023-05-22 09:10:40 +0700

    db.json

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,5 +1,9 @@
+import data from './db.json' assert {type: 'json'}
+
 var app = angular.module("app", [])
 
 app.controller('myController', function ($scope) {
     $scope.greeting = "Hello world"
 })
+
+console.log(data);

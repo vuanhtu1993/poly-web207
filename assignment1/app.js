92de9a36a50bc6dd9062f64623b064fb76aa52ff
tuva9 <tuva9@fpt.edu.vn>
2023-05-28 22:27:32 +0700

    Filter and search

--- assignment1/app.js
+++ assignment1/app.js
@@ -4,4 +4,21 @@ var app = angular.module("app", [])
 
 app.controller('myController', function ($scope) {
     $scope.data = data
+    $scope.search = function(e) {
+        if(e.keyCode == 13) {
+            $scope.data = data.filter(function(item) {
+                return item.name.toLowerCase().includes($scope.searchStr.toLowerCase())
+            })
+        }
+    }
+    $scope.filterIncrease = function() {
+        data.sort(function(a, b) {
+            return b.list_price - a.list_price
+        })
+    }
+    $scope.filterDecrease = function() {
+        data.sort(function(a, b) {
+            return a.list_price - b.list_price
+        })
+    }
 })

7bb82c5511ad8cf6bdadd60a475a247f0040b89d
tuva9 <tuva9@fpt.edu.vn>
2023-05-29 07:57:18 +0700

    Config router

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,7 +1,27 @@
 import data from './db.json' assert {type: 'json'}
 
-var app = angular.module("app", [])
+var app = angular.module("app", ['ngRoute'])
+// Bước1: import angular-route
+// Bước2: Khai báo router
+// Bước3: Khai báo <ng-view></ng-view>
+
+// Router
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: "./pages/home.html",
+            controller: "myController"
+        })
+        .when('/product', {
+            templateUrl: "./pages/product.html",
+            controller: "myController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+})
 
 app.controller('myController', function ($scope) {
     $scope.data = data
 })
+

ddfe23c8b51c99fe964bea6202d9490b7a6658b1
tuva9 <tuva9@fpt.edu.vn>
2023-06-14 07:40:48 +0700

    Base project

--- assignment1/app.js
+++ assignment1/app.js
@@ -8,96 +8,12 @@ app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix("")
     $routeProvider
         .when('/', {
-            templateUrl: "./pages/home.html",
+            // templateUrl
+            template: `<h1 class="text-bg-success">Base project</h1>`,
             controller: "myController"
         })
-        .when('/product/:id', {
-            templateUrl: "./pages/product.html",
-            controller: "productController"
-        })
-        .when('/admin', {
-            templateUrl: "./pages/dashboard.html",
-            controller: "dashboardController"
-        })
-        .when('/admin/add', {
-            templateUrl: "./pages/add-product.html",
-            controller: "addProductController"
-        })
-        .when('/admin/product/:id', {
-            templateUrl: "./pages/update-product.html",
-            controller: "updateProductController"
-        })
-        .otherwise({
-            redirectTo: "/"
-        })
 })
 
-app.controller('myController', function ($rootScope, $scope, $http) {
-    $http.get('http://localhost:3000/books')
-        .then(function (res) {
-            $rootScope.data = res.data
-        }) //Promise
-    $rootScope.books = $rootScope.data
-    $scope.orderProduct = function (type) {
-        $scope.orderType = type
-    }
-    $scope.search = function (event) {
-        if (event.keyCode == 13) {
-            $rootScope.books = $rootScope.data.filter(function (book) {
-                return book.name.toLowerCase().includes($scope.searchStr.toLowerCase())
-            })
-        }
-    }
-})
-
-app.controller("productController", function ($scope, $routeParams, $http) {
-    $scope.id = $routeParams.id
-    // $scope.book = data.find(function (item) {
-    //     return $routeParams.id == item.id
-    // })
-    $http.get('http://localhost:3000/books/' + $scope.id)
-        .then(function (res) {
-            $scope.book = res.data
-        })
-})
-
-
-app.controller('dashboardController', function ($scope, $rootScope, $http) {
-    // $scope.books = $rootScope.books
-    $http.get('http://localhost:3000/books')
-        .then(function (res) {
-            $scope.books = res.data
-        })
-    $scope.delete = function (id) {
-        console.log(id);
-        $http.delete('http://localhost:3000/books/' + id)
-            .then(function () {
-                alert("Xoá thành công")
-            })
-    }
-})
-
-app.controller("addProductController", function ($scope, $routeParams, $http) {
-    $scope.submit = function (event) {
-        if ($scope.product) {
-            $http.post("http://localhost:3000/books", $scope.product)
-                .then(function () {
-                    alert("Thêm mới thành công")
-                })
-        }
-    }
-})
-
-app.controller("updateProductController", function ($scope, $routeParams, $http) {
-    $scope.id = $routeParams.id
-    $http.get("http://localhost:3000/books/" + $scope.id)
-        .then(function (res) {
-            $scope.product = res.data
-        })
-    $scope.submit = function () {
-        $http.put("http://localhost:3000/books/" + $scope.id, $scope.product)
-            .then(function () {
-                alert("Sửa sản phẩm thành công")
-            })
-    }
+app.controller("myController", function () {
+    console.log("myController");
 })
